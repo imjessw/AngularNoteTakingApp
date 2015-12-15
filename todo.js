47abbbdaf0c35c74385f9a166626554c617fc1b7
ImJessW <ImJessW@gmail.com>
2015-12-15 15:20:27 -0500

    added style sheet, added at to todos, added clear input

--- todo.js
+++ todo.js
@@ -2,7 +2,12 @@ function TodoCtrl($scope){
 	
 	$scope.totalTodos =4;
 
-	scope.todos = [{text:"LearnAngular", done:false},
+	$scope.todos = [{text:"LearnAngular", done:false},
 	{text:"Build an App", done:false}
 	];
+
+	$scope.addTodo= function (argument) {
+		$scope.todos.push({text:$scope.formTodoText, done:false});
+		$scope.formTodoText="";
+	}
 }
