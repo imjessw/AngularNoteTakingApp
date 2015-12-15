78018603b10a2dc1ebd8148c01e85283bdfaa1f5
ImJessW <ImJessW@gmail.com>
2015-12-15 15:22:26 -0500

    this checks the length of the array one time

--- todo.js
+++ todo.js
@@ -1,10 +1,11 @@
 function TodoCtrl($scope){
-	
-	$scope.totalTodos =4;
+
 
 	$scope.todos = [{text:"LearnAngular", done:false},
 	{text:"Build an App", done:false}
 	];
+
+	$scope.totalTodos=$scope.todos.length;
 
 	$scope.addTodo= function (argument) {
 		$scope.todos.push({text:$scope.formTodoText, done:false});
